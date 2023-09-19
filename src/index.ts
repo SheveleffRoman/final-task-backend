7d3ab13cc1ea2d8b72dcc7930ec2bfb73d2baea4
Roman <@>
2023-09-19 18:14:05 +0400

    fix: fix path

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import * as serverService from './services/server.service';
 
 (async () => {
   try {
-    await mongoose.connect('mongodb+srv://sheveleff:MVbyGV7L4gd8yYAI@cluster0.rrkktgf.mongodb.net/?retryWrites=true&w=majority"');
+    await mongoose.connect('mongodb+srv://sheveleff:MVbyGV7L4gd8yYAI@cluster0.rrkktgf.mongodb.net/?retryWrites=true&w=majority');
     serverService.server.listen(process.env.PORT || PORT, function () {
       console.log('Сервер ожидает подключения...');
     })
